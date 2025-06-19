4ba6fde0e84f928b4b778e4868b2b011a7340ec2
Ronald <ron.maroria@gmail.com>
2025-06-19 00:12:59 -0500

    commit

--- synthesize.py
+++ synthesize.py
@@ -12,42 +12,48 @@ from datetime import datetime
 
 polly_client = boto3.client('polly')
 s3_client = boto3.client('s3')
+converted_file = ""
 
-def convert_text_to_speech(input_text_file_path: str):
+def convert_text_to_speech(input_text_file_path, converted_file):
     """
     Converts text to speech using Amazon Polly
     """
     try:
-        with open(input_text_file_path, 'r') as file: 
+        with open(input_text_file_path, 'w') as file: 
             text = file.read()
-            output_file = polly_client.synthesize_speech(
+            converted_file = polly_client.synthesize_speech(
                 Text=text,
                 OutPutFormat='mp3',
                 VoiceId='Joanna'
             )
-        return output_file
+        file.write(converted_file)
     except:
         print("Error converting text to speech")
         return None
     
-def upload_to_s3(bucket_name: str, output_file: str,key: str):
-        """
+def upload_to_s3(bucket_name: str, write_file: str, key: str):
+    """
     Uploads audio file to S3 bucket
     """
-try:
-        with open(output_file, 'wb') as file:
+    try:
+        with open(write_file, 'rb') as file:
             write_file = s3_client.put_object(
-            bucket = bucket_name,
+            Bucket = bucket_name,
             key = key,
             body = file
             )
-except Exception as e:
+        return write_file
+    except Exception as e:
         print(f'"{e}Error uploading to S3", e')
-        
+
+def main():
+    convert_text_to_speech(input_text_file_path="speech.txt", converted_file=converted_file)
+    upload_to_s3(bucket_name="polly-demo-2025-luit-jun10", converted_file=converted_file, key="audio/"+str(datetime.now())+".mp3") 
+
 
 if __name__ == "__main__":
-      convert_text_to_speech(sys.argv[0])
-      upload_to_s3   
+    main()
+        
     
 
 
